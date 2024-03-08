0d7c3802f168f493e9e9148663e9a261f7f695fe
BelkacemYerfa <belkacem.yerfa@gmail.com>
2024-03-08 03:34:58 +0100

    add : the search popup for new docs

--- packages/src/components/layouts/site-header.tsx
+++ packages/src/components/layouts/site-header.tsx
@@ -4,10 +4,11 @@ import { Balancer } from "react-wrap-balancer";
 import { siteConfig } from "@/config/site-config";
 import { SocialLinks } from "../social-links";
 import { ModeToggle } from "../toggle-theme";
+import { SearchPopOver } from "../search";
 
 export const SiteHeader = () => {
   return (
-    <header className="flex items-center justify-between px-4 py-2  max-w-screen-2xl h-12">
+    <header className="flex items-center justify-between px-4 py-2  max-w-screen-2xl h-12 border-b border-border">
       <nav className="flex items-center justify-between w-full">
         <div className="flex items-center gap-4">
           <Link href="/" className="flex items-center gap-2">
@@ -29,9 +30,12 @@ export const SiteHeader = () => {
             Components
           </Link>
         </div>
-        <SocialLinks className="hidden xs:flex" />
-        <div className="flex xs:hidden">
-          <ModeToggle />
+        <div className="flex items-center gap-1 xs:gap-2">
+          <SearchPopOver />
+          <SocialLinks className="hidden xs:flex" />
+          <div className="flex xs:hidden">
+            <ModeToggle />
+          </div>
         </div>
       </nav>
     </header>
--- packages/src/lib/utils.ts
+++ packages/src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const isMacOs = () => {
+  if (typeof window === "undefined") return false;
+  return window.navigator.userAgent.includes("Mac");
+};
