9818b032a8132b6698c0a39172c69704618716ac
Gaurang Shah <gaurang.r.shah@gmail.com>
2024-04-11 14:48:30 -0400

    fix: increase opacity of inactive thumb item

--- src/registry/default/extension/carousel.tsx
+++ src/registry/default/extension/carousel.tsx
@@ -300,7 +300,7 @@ const SliderThumbItem = forwardRef<
       )}
     >
       <div
-        className={`relative aspect-square h-20 w-full opacity-40 rounded-md transition-opacity ${
+        className={`relative aspect-square h-20 w-full opacity-50 rounded-md transition-opacity ${
           isSlideActive ? "!opacity-100" : ""
         }`}
       >
