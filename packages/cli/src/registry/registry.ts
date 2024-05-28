c80927f3530291cf815299904dff38ce364add4e
Raphael-08 <mohanvarma130@gmail.com>
2024-05-28 19:34:25 +0530

    removed unwanted files

--- packages/cli/src/registry/registry.ts
+++
@@ -1,5 +0,0 @@
-import { Registry } from "@/registry/schema";
-import { ui } from "@/registry/ui";
-
-// Exporting the registry directly from the `ui` file
-export const registry: Registry = [...ui];
