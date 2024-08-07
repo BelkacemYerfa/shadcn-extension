ab5bcf298c4f0f4d8293cc87a9e8f4fd3fcf342d
Raphael-08 <mohanvarma130@gmail.com>
2024-08-07 20:15:12 +0530

    delete

--- src/components/provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ThemeProvider } from "next-themes";
-
-export const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
-};
--- src/registry/default/example/smart-datetime-input-demo.tsx
+++
@@ -1,7 +0,0 @@
-import { SmartDatetimeInput } from "@/registry/default/extension/smart-datetime-input";
-
-const SmartDateTimeInputDemo = () => {
-  return <SmartDatetimeInput onValueChange={console.log} />;
-};
-
-export default SmartDateTimeInputDemo;
