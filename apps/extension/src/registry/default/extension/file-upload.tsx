8500f3d9948a194601aa804444670ffe4d09fced
Yerfa Belkacem <106016365+BelkacemYerfa@users.noreply.github.com>
2025-03-31 18:04:19 +0100

    fix(app): issue #120 bug resulted in removing the unlimited files upload

--- apps/extension/src/registry/default/extension/file-upload.tsx
+++ apps/extension/src/registry/default/extension/file-upload.tsx
@@ -22,6 +22,7 @@ import {
 import { toast } from "sonner";
 import { Trash2 as RemoveIcon } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
+import { max } from "date-fns";
 
 type DirectionOptions = "rtl" | "ltr" | undefined;
 
@@ -68,13 +69,13 @@ export const FileUploader = forwardRef<
       dropzoneOptions,
       value,
       onValueChange,
-      reSelect,
+      reSelect = false,
       orientation = "vertical",
       children,
       dir,
       ...props
     },
-    ref,
+    ref
   ) => {
     const [isFileTooBig, setIsFileTooBig] = useState(false);
     const [isLOF, setIsLOF] = useState(false);
@@ -83,7 +84,7 @@ export const FileUploader = forwardRef<
       accept = {
         "image/*": [".jpg", ".jpeg", ".png", ".gif"],
       },
-      maxFiles = 1,
+      maxFiles = 0,
       maxSize = 4 * 1024 * 1024,
       multiple = true,
     } = dropzoneOptions;
@@ -97,7 +98,7 @@ export const FileUploader = forwardRef<
         const newFiles = value.filter((_, index) => index !== i);
         onValueChange(newFiles);
       },
-      [value, onValueChange],
+      [value, onValueChange]
     );
 
     const handleKeyDown = useCallback(
@@ -153,7 +154,7 @@ export const FileUploader = forwardRef<
         }
       },
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      [value, activeIndex, removeFileFromSet],
+      [value, activeIndex, removeFileFromSet]
     );
 
     const onDrop = useCallback(
@@ -172,7 +173,11 @@ export const FileUploader = forwardRef<
         }
 
         files.forEach((file) => {
-          if (newValues.length < maxFiles) {
+          if (maxFiles > 0) {
+            if (newValues.length < maxFiles) {
+              newValues.push(file);
+            }
+          } else {
             newValues.push(file);
           }
         });
@@ -183,7 +188,7 @@ export const FileUploader = forwardRef<
           for (let i = 0; i < rejectedFiles.length; i++) {
             if (rejectedFiles[i].errors[0]?.code === "file-too-large") {
               toast.error(
-                `File is too large. Max size is ${maxSize / 1024 / 1024}MB`,
+                `File is too large. Max size is ${maxSize / 1024 / 1024}MB`
               );
               break;
             }
@@ -195,12 +200,12 @@ export const FileUploader = forwardRef<
         }
       },
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      [reSelectAll, value],
+      [reSelectAll, value]
     );
 
     useEffect(() => {
       if (!value) return;
-      if (value.length === maxFiles) {
+      if (value.length === maxFiles && maxFiles > 0 && multiple) {
         setIsLOF(true);
         return;
       }
@@ -240,7 +245,7 @@ export const FileUploader = forwardRef<
             className,
             {
               "gap-2": value && value.length > 0,
-            },
+            }
           )}
           dir={dir}
           {...props}
@@ -249,7 +254,7 @@ export const FileUploader = forwardRef<
         </div>
       </FileUploaderContext.Provider>
     );
-  },
+  }
 );
 
 FileUploader.displayName = "FileUploader";
@@ -273,7 +278,7 @@ export const FileUploaderContent = forwardRef<
         className={cn(
           "flex rounded-xl gap-1",
           orientation === "horizontal" ? "flex-raw flex-wrap" : "flex-col",
-          className,
+          className
         )}
       >
         {children}
@@ -297,7 +302,7 @@ export const FileUploaderItem = forwardRef<
         buttonVariants({ variant: "ghost" }),
         "h-6 p-1 justify-between cursor-pointer relative",
         className,
-        isSelected ? "bg-muted" : "",
+        isSelected ? "bg-muted" : ""
       )}
       {...props}
     >
@@ -308,7 +313,7 @@ export const FileUploaderItem = forwardRef<
         type="button"
         className={cn(
           "absolute",
-          direction === "rtl" ? "top-1 left-1" : "top-1 right-1",
+          direction === "rtl" ? "top-1 left-1" : "top-1 right-1"
         )}
         onClick={() => removeFileFromSet(index)}
       >
@@ -345,7 +350,7 @@ export const FileInput = forwardRef<
                ? "border-red-500"
                : "border-gray-300"
          }`,
-          className,
+          className
         )}
         {...rootProps}
       >
